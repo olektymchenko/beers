4290638cbbed424e109cd573080c316d44f301b8
olektymchenko <58951697+olektymchenko@users.noreply.github.com>
2020-03-28 17:17:22 +0100

    Updates

---
+++ precache-manifest.1a15a9f398b707dc87665316f9fb2cc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73fcd849ac6b74bae50aca578025e3c3",
+    "url": "/beers/index.html"
+  },
+  {
+    "revision": "e44fc818cf792250cf89",
+    "url": "/beers/static/css/main.b1c39015.chunk.css"
+  },
+  {
+    "revision": "c8ce03107cecab1a6c04",
+    "url": "/beers/static/js/2.69394aa4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/beers/static/js/2.69394aa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e44fc818cf792250cf89",
+    "url": "/beers/static/js/main.37b9ece7.chunk.js"
+  },
+  {
+    "revision": "6ab7eb77008cdb3b7734",
+    "url": "/beers/static/js/runtime-main.cf4cdbec.js"
+  }
+]);
